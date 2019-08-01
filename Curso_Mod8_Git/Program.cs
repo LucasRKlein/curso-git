fc9ebdb42cd2f1abc91e9125075364d17d78df5c
Lucas Klein <lucasrklein@gmail.com>
2019-08-01 11:13:53 -0300

    projeto iniciado, mostrando mensagem ola mundo

---
+++ Curso_Mod8_Git/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Curso_Mod8_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+      
+            Console.ReadKey();
+        }
+    }
+}
